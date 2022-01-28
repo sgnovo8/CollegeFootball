2309d4d2d9d922d8b193b346dfbe9b162573380e
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2022-01-28 09:31:49 -0500

    Create CollegeFootball.sol

---
+++ CollegeFootball.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+contract StateFootball {
+
+    string state;
+    string team;
+    string mascot;
+    string coach;
+
+    function addState(string memory _state) public{
+        state = _state;
+    }
+
+    function returnState() public view returns (string memory _state){
+        return state;
+    }
+
+    function addTeam(string memory _team) public{
+        team = _team;
+    }
+    function getTeam() public view returns(string memory _team){
+        return team;
+    }
+}
